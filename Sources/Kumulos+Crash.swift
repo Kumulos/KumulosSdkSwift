ec0468be20ea1a39c57c56e07f8df872d2b0b813
Andrew Lindsay <a.lindsay@kumulos.com>
2017-10-05 17:13:05 +0100

    Expose helper method for logging manual exceptions immediately.

--- Sources/Kumulos+Crash.swift
+++ Sources/Kumulos+Crash.swift
@@ -31,7 +31,17 @@ public extension Kumulos {
                 print("Failed to send reports: \(String(describing: error))")
             }
         }
+    }
+    
+    public static func logException(name: String, reason: String, language: String, lineOfCode: String, stackTrace: [Any], logAllThreads: Bool)  {
+        KSCrash.sharedInstance().reportUserException(name, reason: reason, language: language, lineOfCode: lineOfCode, stackTrace: stackTrace, logAllThreads: logAllThreads, terminateProgram: false)
         
-        
+        KSCrash.sharedInstance().sendAllReports{ (reports, completed, error) -> Void in
+            if(completed) {
+                print("Sent \(String(describing: reports?.count)) reports")
+            } else {
+                print("Failed to send reports: \(String(describing: error))")
+            }
+        }
     }
 }
