1a541e9e44923e9f157e7813324e89b1e6f46ee4
Christopher Wyllie <c.wyllie@kumulos.com>
2018-01-10 11:56:32 +0000

    Add analytics public API shape

---
+++ Sources/Kumulos+Analytics.swift
@@ -0,0 +1,33 @@
+//
+//  Kumulos+Analytics.swift
+//  KumulosSDK
+//
+//  Copyright © 2018 Kumulos. All rights reserved.
+//
+
+import Foundation
+
+public extension Kumulos {
+    
+    /**
+     Logs an analytics event to the local database
+     
+     Parameters:
+     - eventType: Unique identifier for the type of event
+     - properties: Optional meta-data about the event
+     */
+    public static func trackEvent(eventType: String, properties: AnyObject?) {
+        // TODO
+    }
+    
+    /**
+     Associates a user identifier with the current Kumulos installation record
+     
+     Parameters:
+     - userIdentifier: Unique identifier for the current user
+     */
+    public static func associateUserWithInstall(userIdentifier: String) {
+        // TODO
+    }
+    
+}
