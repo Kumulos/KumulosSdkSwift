2991e9a3a82dcdd73be40d19364232a55b6bc90c
Andrew Lindsay <a.lindsay@kumulos.com>
2019-09-25 10:42:51 +0100

    Port some code.

--- Kumulos+InApp.swift
+++ Kumulos+InApp.swift
@@ -20,7 +20,23 @@ public extension Kumulos {
         
     }
     
-    func presentInboxMessage(inAppMessage: InAppMessage) -> InAppMessagePresentationResult {
-        
+
+    func updateConsent(forUser consentGiven: Bool) {
+        if self.inAppConsentStrategy != InAppConsentStrategy.ExplicitByUser {
+            NSException.raise("Kumulos: Invalid In-app consent strategy", format: "You can only manage in-app messaging consent when the feature is enabled and strategy is set to KSInAppConsentStrategyExplicitByUser")
+            return
+        }
+
+        self.inAppHelper.updateUserConsent(consentGiven: consentGiven)
+    }
+    
+    class func presentInboxMessage(_ item: InAppInboxItem?) -> InAppMessagePresentationResult {
+        if item?.isAvailable() == nil {
+            return InAppMessagePresentationResult.EXPIRED
+        }
+
+        let result = self.inAppHelper.presentMessage(withId: item?.id)
+
+        return result ? InAppMessagePresentationPresented : InAppMessagePresentation.FAILED as! InAppMessagePresentationResult
     }
 }
