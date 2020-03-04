4c705f940a410b082ee3560c565a8262cffad8e5
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-03-04 11:28:22 +0000

    bump SDK version

--- Sources/Kumulos.swift
+++ Sources/Kumulos.swift
@@ -61,7 +61,7 @@ open class Kumulos {
     internal let pushNotificationDeviceType = 1
     internal let pushNotificationProductionTokenType:Int = 1
     
-    internal let sdkVersion : String = "8.2.3"
+    internal let sdkVersion : String = "8.2.4"
 
     var networkRequestsInProgress = 0
 
