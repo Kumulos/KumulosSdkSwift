88ed9dd8afa72eb09d474c2a4663a0456b2a3cc0
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-03-26 15:35:23 +0000

    remove copied header

--- Sources/KSBadgeObserver.m
+++ Sources/KSBadgeObserver.m
@@ -1,12 +1,3 @@
-//
-//  MobileProvision.m
-//  From https://github.com/OneSignal/OneSignal-iOS-SDK/tree/master/iOS_SDK/OneSignalMobileProvision
-//  Renamed from UIApplication+BSMobileProvision.m to prevent conflicts
-//
-//  Created by kaolin fire on 2013-06-24.
-//  Copyright (c) 2013 The Blindsight Corporation. All rights reserved.
-//  Released under the BSD 2-Clause License (see LICENSE)
-
 #import "KSBadgeObserver.h"
 #import <UIKit/UIKit.h>
 
