11ff32c5fb03c9adf226644e3756fe733d99730a
Christopher Wyllie <c.wyllie@kumulos.com>
2020-10-15 15:02:35 +0100

    Bump version

--- Sources/Kumulos.swift
+++ Sources/Kumulos.swift
@@ -56,7 +56,7 @@ open class Kumulos {
     internal let pushNotificationDeviceType = 1
     internal let pushNotificationProductionTokenType:Int = 1
 
-    internal let sdkVersion : String = "8.5.0"
+    internal let sdkVersion : String = "8.6.0"
 
     var networkRequestsInProgress = 0
 
