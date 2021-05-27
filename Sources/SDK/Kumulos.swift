5310da2bb2722c3561ac243509cadb22c1f803cd
vvoicehovics <vladislav.voicehovich@gmail.com>
2021-05-27 13:00:46 +0300

    bump versions

--- Sources/SDK/Kumulos.swift
+++ Sources/SDK/Kumulos.swift
@@ -59,7 +59,7 @@ open class Kumulos {
     internal let pushNotificationDeviceType = 1
     internal let pushNotificationProductionTokenType:Int = 1
 
-    internal let sdkVersion : String = "8.11.1"
+    internal let sdkVersion : String = "8.11.2"
 
     var networkRequestsInProgress = 0
 
