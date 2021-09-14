25ded589dd88400b1ed30c9a048cf369c1885cdd
Andrew Lindsay <a.lindsay@kumulos.com>
2021-09-14 14:15:43 +0100

    Add iOS15 guards and get SDK happy building.

--- Sources/Extension/KumulosNotificationService.swift
+++ Sources/Extension/KumulosNotificationService.swift
@@ -88,27 +88,32 @@ public class KumulosNotificationService {
             let id = buttonDict["id"] as! String
             let text = buttonDict["text"] as! String
             
-            let icon = getButtonIcon(button: buttonDict)
+            if #available(iOSApplicationExtension 15.0, *) {
+                let icon = getButtonIcon(button: buttonDict)
+                let action = UNNotificationAction(identifier: id, title: text, options: .foreground, icon: icon)
+                actionArray.add(action);
+            } else {
+                let action = UNNotificationAction(identifier: id, title: text, options: .foreground)
+                actionArray.add(action);
+            }
             
-            //TODO - Necessary to guard this constructor with 15+ ?
-            let action = UNNotificationAction(identifier: id, title: text, options: .foreground, icon: icon)
-            actionArray.add(action);
         }
 
         return actionArray;
     }
     
-    fileprivate class func getButtonIcon(button:AnyHashable:Any]) -> UNNotificationActionIcon? {
-        guard buttonDict["icon"] != nil else {
+    @available(iOSApplicationExtension 15.0, *)
+    fileprivate class func getButtonIcon(button:[AnyHashable:Any]) -> UNNotificationActionIcon? {
+        guard button["icon"] != nil else {
             return nil
         }
         
-        let iconDict = buttonDict["icon"] as! [String:String]
+        let iconDict = button["icon"] as! [String:String]
         
-        let iconType = iconDict["type"] as !String
-        let iconId = iconDict["iconId"] as !String
+        let iconType = iconDict["type"] as! String
+        let iconId = iconDict["iconId"] as! String
         
-        if (iconType === "custom") {
+        if (iconType == "custom") {
             //TODO - What if this doesnt exist? Catch exception -> return nil?
             return UNNotificationActionIcon(templateImageName: iconId)
         }
